321c9f699f6cfa1cf23ea06d64eeee97ddde9754
abdoulaziba <baabdoulba770@gmail.com>
2024-08-05 18:31:55 +0000

    feat: spark numeric

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { UserProvider } from '../context/UserContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
+
+export default MyApp;
+
